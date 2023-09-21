e821d3427d05d9e9e9c1b77b48e4f924881b9d88
Ken Van Goethem <ken.van.goethem@hotmail.com>
2023-09-21 21:33:30 +0200

    added QualityConstants

---
+++ src/main/java/com/gildedtros/QualityConstants.java
@@ -0,0 +1,14 @@
+package com.gildedtros;
+
+public class QualityConstants {
+
+    static final String POOR = "POOR";
+    static final String COMMON = "COMMON";
+
+    static final String WINE = "WINE";
+    static final String BACKSTAGE_PASS = "BACKSTAGE_PASS";
+
+    static final String LEGENDARY = "LEGENDARY";
+
+
+}
